9f86454f4488b7600ff2c8372b11534204b0f610
Dave Lunny <sir.dave@me.com>
2014-10-10 02:19:30 -0400

    some pretty groovy updates (found out why GSAP was being such a little bitch)
    GSAP doesn't like .trans(all..) for some reason. switching it to a one-
    only transition fixed it.

--- js/app.js
+++ js/app.js
@@ -10,28 +10,79 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	    .state('home', {
 	      url: "/",
 	      templateUrl: "partials/view.html",
-	      controller: "Controller"
+	      controller: "MainCtrl"
 	    })
 });
 
 
-app.controller('Controller', function ($scope) {
+app.controller('MainCtrl', function ($scope, $window) {
+
+	var h = $(window).height();
+	var animOccured = false;
+	$(window).scroll(function(){
+		var s = $(window).scrollTop();
+		if(!animOccured){
+			if( (s/h*100) > 52  ){
+				TweenLite.to( $('section#skills'), 1.6, { maxWidth: "500px" });
+				animOccured = true;
+			}
+		}
+
+	});
+
 
 	function init(){
-		var tl = new TimelineMax();
 
 		setTimeout(function(){
 			var face = $('#face'), 
 				hmnid = $('#hmnid'), 
 				info = $('#info'); 
 
+			var tl = new TimelineMax();
 			//append a to() tween
-			tl.to(face, 1.6, {top:"10%", scale: 1});
-			tl.to(hmnid, 0.8, { scale: 1, opacity: 1 });
-			tl.to(info, 0.8, { scale: 1, opacity: 1 });
+			tl.to(face, 1.7, {left:"50%", rotation: 360, ease: Back.easeOut });
+			tl.to(hmnid, 0.65, { scale: 1, opacity: 1, ease: Back.easeOut });
+			tl.to(info, 0.65, { scale: 1, opacity: 1, ease: Back.easeOut });
 
-		},200);
+		},100);
+
+
+
+
 	};
+
+	//later move to le json
+	$scope.skills = [
+							{
+								"skill"	: 	"HTML5",
+								"level"	: 	10
+							}
+						,	{
+								"skill"	: 	"CSS/LESS/SASS",
+								"level"	: 	10
+							}
+						,	{
+								"skill"	: 	"Angular.JS",
+								"level"	: 	6
+							}
+						,	{
+								"skill"	: 	"Photoshop/Illustrator",
+								"level"	: 	10
+							}
+						,	{
+								"skill"	: 	"Bootstrap",
+								"level"	: 	7
+							}
+						,	{
+								"skill"	: 	"Git",
+								"level"	: 	5
+							}
+						,	{
+								"skill"	: 	"Node.js",
+								"level"	: 	4
+							}
+					];
+
 
 	$(document).ready(init)
 
